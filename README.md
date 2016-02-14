e2c2cb34b95603a818b0208ca0e0fd22725d637c
qll <qll@iceqll.eu>
2016-02-14 23:56:42 +0100

    Add prototype

---
+++ README.md
@@ -0,0 +1,10 @@
+# ezlist #
+If you have been looking for a mailing list manager, you know the problem:
+Either you use extremely overfeatured software like mailman or you end up with a
+strange PHP script somebody hacked together. I hope that ezlist will some day be
+a real alternative for those who do not have to manage a huge mailing list and
+do not require many of the more advanced features of such software. However,
+right now ezlist is strictly experimental albeit working. I will write
+documentation at a later point...
+
+This script requires Python 3 to work properly.
---
+++ settings.example.py
@@ -0,0 +1,72 @@
+POLLING_INTERVAL = 20
+
+
+MANAGER_CLASS = 'Manager'
+MANAGER_ARGS = {
+    'mail_addr': 'mailinglist@anything.com',
+    'manage_subscriptions': True,
+    'subject_prefix': '[List]',
+}
+
+
+STORAGE_CLASS = 'SQLiteStorage'
+STORAGE_ARGS = {
+    'path': 'data.sqlite',
+}
+
+
+INBOX_CLASS = 'IMAPInbox'
+INBOX_ARGS = {
+    'host': 'anything.com',
+    'port': 143,
+    'username': 'mailinglist',
+    'password': 'something',
+    'inbox': 'INBOX',
+    'ssl': False,
+    'startssl': False,
+}
+
+
+SENDER_CLASS = 'SMTPSender'
+SENDER_ARGS = {
+    'host': 'anything.com',
+    'port': 25,
+    'domain': 'anything.com',
+    'username': 'mailinglist',
+    'password': 'something',
+    'ssl': False,
+    'startssl': False,
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '[%(levelname)s]%(asctime)s: %(message)s',
+            'datefmt': '%d.%m.%Y/%H:%S',
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        # this probably is what you'd want in a daemonized setting:
+        # 'file': {
+        #     'level': 'INFO',
+        #     'class': 'logging.FileHandler',
+        #     'filename': 'storage/bot.log',
+        #     'formatter': 'standard'
+        # }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+}
